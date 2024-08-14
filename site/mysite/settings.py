59be20dfc04dd84bcf00741b335028cc2a991348
hmelica <hippolytemelica@icloud.com>
2024-08-14 16:15:21 +0200

    django and nginx running together

--- site/mysite/settings.py
+++ site/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5wkco=$x@@cy3^#tblhbqs%p5z#s++kw^p2)!+*bl*^=n(v_o0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition

3bc9cd856fbc07469ef5fa64a65f0d5d7096c00c
hmelica <hippolytemelica@icloud.com>
2024-08-15 17:05:20 +0200

    Adding proper verbose to decrypt and encrypt scripts

--- scripts/decrypt_env.sh
+++ scripts/decrypt_env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ -f env_file ]; then rm -f $2 ; fi
 gpg --output $2 --decrypt --batch --passphrase-file=.passwd $1
-echo ".env file has been written to " $2
+echo "$1 file has been written to $2"
--- scripts/encrypt_env.sh
+++ scripts/encrypt_env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ -f srcs/.env ]; then rm -f $2 ; fi
 gpg --output $2 --symmetric --batch --yes --passphrase-file=.passwd $1
-echo "srcs/.env file has been saved as env_file"
+echo "$1 file has been saved as $2"

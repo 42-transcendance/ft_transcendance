09b39c2d7455e21305eb39b29749b1193fd57862
hmelica <hippolytemelica@icloud.com>
2024-08-15 14:20:35 +0200

    protecting django secrets and removing .env from repo........

--- site/mysite/settings.py
+++ site/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +21,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5wkco=$x@@cy3^#tblhbqs%p5z#s++kw^p2)!+*bl*^=n(v_o0'
+if (environ.get("DJANGO_SECRET_KEY") == None):
+    print("DJANGO_SECRET_KEY is not safe, django can not start safely")
+    exit(1)
+SECRET_KEY = environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if (environ.get("DEBUG") != None):
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-
+if (environ.get("HOST") != None):
+    ALLOWED_HOSTS = environ.get("HOST").split(" ")
+else:
+    ALLOWED_HOSTS = []
 
 # Application definition
 

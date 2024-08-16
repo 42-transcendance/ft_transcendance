584368ea4ec0f804104037a28b795e56e75d5291
hmelica <hippolytemelica@icloud.com>
2024-08-16 14:00:23 +0200

    django project can now auto start

--- srcs/requirements/django/run.sh
+++ srcs/requirements/django/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [ ! -f manage.py ]; then
+    echo manage.py not found, creating project
+    django-admin startproject $NAME .
+fi
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:$INTERNAL_DJANGO_PORT
